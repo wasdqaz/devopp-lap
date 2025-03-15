0c26f7ad1a9b222e9c39a8bcfc092d1b9b9a0725
LeBaoGiang <126789173+LeBaoGiang@users.noreply.github.com>
2025-03-15 19:46:09 +0700

    Add simple test for CustomerController

---
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/CustomerControllerTest.java
@@ -0,0 +1,22 @@
+package org.springframework.samples.petclinic.customers.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CustomerController.class)
+public class CustomerControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetCustomerById() throws Exception {
+        mockMvc.perform(get("/customers/1"))
+               .andExpect(status().isOk());
+    }
+}
