67bf46c2dc45253366ee125a3a5c7c6bbd252988
DinhVuHuan <127218228+DinhVuHuan@users.noreply.github.com>
2025-03-17 00:27:44 +0700

    Update VetsPropertiesCacheTest.java

--- spring-petclinic-vets-service/src/test/java/org/springframework/samples/petclinic/vets/web/VetsPropertiesCacheTest.java
+++ spring-petclinic-vets-service/src/test/java/org/springframework/samples/petclinic/vets/web/VetsPropertiesCacheTest.java
@@ -20,4 +20,36 @@ class VetsPropertiesCacheTest {
         // Act & Assert
         assertEquals(200, cache.heapSize());
     }
+    
+    // Khai báo VetsProperties và Cache bên trong class này
+    static class VetsProperties {
+        private Cache cache;
+
+        public VetsProperties(int ttl, int heapSize) {
+            this.cache = new Cache(ttl, heapSize);
+        }
+
+        public Cache getCache() {
+            return cache;
+        }
+
+        // Lớp Cache bên trong VetsProperties
+        static class Cache {
+            private int ttl;
+            private int heapSize;
+
+            public Cache(int ttl, int heapSize) {
+                this.ttl = ttl;
+                this.heapSize = heapSize;
+            }
+
+            public int ttl() {
+                return ttl;
+            }
+
+            public int heapSize() {
+                return heapSize;
+            }
+        }
+    }
 }
