9449d94bc4ffd218a314effaaaae540c29475db2
LeBaoGiang <126789173+LeBaoGiang@users.noreply.github.com>
2025-03-16 19:48:06 +0700

    Update OwnerResourceTest.java

--- spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/OwnerResourceTest.java
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/OwnerResourceTest.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.springframework.samples.petclinic.customers.model.Pet;
 
