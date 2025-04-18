cb791b6e8239b29c03b01f8b67a5cc3e5bedce7c
phạm anh khôi <126933505+wasdqaz@users.noreply.github.com>
2025-04-18 16:24:35 +0700

    Update SpringBootAdminApplication.java

--- spring-petclinic-admin-server/src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
+++ spring-petclinic-admin-server/src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
@@ -19,7 +19,7 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-// 
+
 @SpringBootApplication
 @EnableAdminServer
 @EnableDiscoveryClient
