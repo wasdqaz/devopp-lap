2fe42aae01401ebe7b23344f7f13dddb71a87c0d
phạm anh khôi <126933505+wasdqaz@users.noreply.github.com>
2025-04-18 17:21:06 +0700

    Update SpringBootAdminApplication.java

--- spring-petclinic-admin-server/src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
+++ spring-petclinic-admin-server/src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 package org.springframework.samples.petclinic.admin;
-
+//
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
